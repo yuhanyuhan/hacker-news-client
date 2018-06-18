99a570976d3986367a3f7cdf274ef75ae9531db8
yuhanyuhan <yuhanlian@gmail.com>
2018-06-18 16:36:19 +0800

    first edit

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
+import {articles} from '../seedData.js'
+import '../index.css'
 
 class NewsFeed extends Component {
   constructor() {
     super();
     this.state = {
-      articles: []
+      articles: articles
     };
   }
 
   render() {
     return this.state.articles.map((article, i) => {
-      // TODO: start coding!
-      return <li key={i}></li>;
+      return (
+        <div id ="eacharticle" key={i}>
+          <p> {article.source.id} by {article.source.name}</p>
+          <p> {article.title} by {article.author}  </p>
+          <p> {article.description} </p>
+          <a href = {article.url}> <img src = {article.urlToImage} alt={"logo"}/></a>
+          <p><button> + </button> {article.votes} </p>
+          <p> {article.publishedAt} </p>
+        </div>
+      );
     });
   }
 }
